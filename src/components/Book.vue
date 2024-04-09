c21ef9849b1986d638c863b7504856b48860dcbb
Zam <samuel.kodehode@gmail.com>
2024-04-09 09:09:01 +0200

    added more data

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,13 +1,3 @@
-<template>
-    <section>
-        <div @click="" v-for="book in books" :key="book.id">
-            <h1>{{ book.title }}</h1>
-            <h1>{{ book.id }}</h1>
-            <img :src="getCoverImageUrl(book)" alt="" />
-        </div>
-    </section>
-</template>
-
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { BookMetadata } from '../types/types.ts'
@@ -33,12 +23,28 @@ const getCoverImageUrl = (book: BookMetadata): string => {
 }
 </script>
 
+<template>
+    <section>
+        <div id="book-wrapper" @click="" v-for="book in books" :key="book.id">
+            <h1>{{ book.title }}</h1>
+            <div v-for="subject in book.subjects">
+                <span>{{ subject }}</span>
+            </div>
+            <h1>{{ book.id }}</h1>
+            <div v-for="author in book.authors">
+                <h3>{{ author.name }}</h3>
+            </div>
+            <img :src="getCoverImageUrl(book)" alt="" />
+        </div>
+    </section>
+</template>
+
 <style scoped>
 h1 {
     font-size: 24px;
     color: #ffffff;
 }
-div {
+#book-wrapper {
     background-color: rgba(83, 91, 242, 0.15);
     width: 20vw;
     padding: 2rem;
